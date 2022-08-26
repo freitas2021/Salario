b1d062e6e01680414900bbbf93052bcb8e1d3cf8
freitas2021 <assis.freitas@ymail.com>
2022-08-26 12:32:18 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿class Salario
+{
+    static void Main()
+    {
+        string nome;
+        string salario;
+        int valorHr = 80;
+        int hrDiaria;
+        bool controler = true;
+        
+
+       
+        Console.WriteLine("Seja bem vindo, por favor seu nome: ");
+        nome = Console.ReadLine();
+        Console.WriteLine($"{nome}. Digite quantas horas deseja trabalhar diariamente: ");
+        hrDiaria = Convert.ToInt32(Console.ReadLine());
+        
+
+        while(controler)
+        {
+
+        Console.WriteLine($"{nome}. Escolha o que deseja saber:\n 1 - Para salário diário;\n 2 - Para salário semanal;\n 3 - Para salário mensal;\n 4 - Para salário Anual;\n ");
+
+        salario = Console.ReadLine();
+
+        switch (salario)
+        {
+            case "1" :
+                salarioDiario(hrDiaria, valorHr);
+                break;
+            case "2" :
+                salarioSemanal(hrDiaria, valorHr);
+                break;
+            case "3" :
+                salarioMensal(hrDiaria, valorHr);
+                break;
+             case "4" :
+                salarioAnual(hrDiaria, valorHr);
+                break;                      
+            default:
+                Console.WriteLine("Opção inválida, por favor escola uma opção válida");
+                break;
+        }
+
+        Console.WriteLine("Aperte qualquer tecla para continuar ou digite [s] para sair.\n");
+        string sair = Console.ReadLine();
+
+        if(sair == "s"){
+            controler = false;
+
+        }
+        
+        }
+    }
+
+    static void salarioDiario(int hrDiaria, int valorHr, string nome)
+    {
+        int res = hrDiaria * valorHr; 
+        Console.WriteLine($"{nome}, o seu salário diário é R${res:F2}\n");
+    }
+    static void salarioSemanal(int hrDiaria, int valorHr, string nome)
+    {
+        int res = (hrDiaria * valorHr) * 5;
+        Console.WriteLine($"{nome}, o seu salário semanal é R${res:F2}\n");
+    }
+    static void salarioMensal(int hrDiaria, int valorHr, string nome)
+    {
+        int res = (hrDiaria * valorHr) * 20;
+        Console.WriteLine($"{nome}, o seu salário semanal é R${res:F2}\n");
+    }
+    static void salarioAnual(int hrDiaria, int valorHr, string nome)
+    {
+        int res = (hrDiaria * valorHr) * 240;
+        Console.WriteLine($"{nome} O seu salário semanal é R${res:F2}\n");
+    }
+}
